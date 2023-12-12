1ddca9129aae1bac5eb548ad9e3b536fbf12a27e
pavan0301 <pthallapalli@gainsight.com>
2023-12-12 16:50:54 +0530

    GainsightAppTag Installation

--- Script.js
+++ Script.js
@@ -1,16 +1,43 @@
-<script>
-        function validateForm() {
-            var username = document.getElementById('username').value;
-            var password = document.getElementById('password').value;
+function validateForm() {
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
 
-            // Simple validation - Check if the fields are not empty
-            if (username.trim() === '' || password.trim() === '') {
-                alert('Username and password are required!');
-            } else {
-                // You can add additional validation or submit the form here
-                alert('Login successful!');
-                window.location.href = 'Page2.html';
-                // document.getElementById('loginForm').submit(); // Uncomment to submit the form
-            }
+        // Simple validation - Check if the fields are not empty
+        if (username.trim() === '' || password.trim() === '') {
+            alert('Username and password are required!');
         }
-</script>
+        else {
+
+            //alert('Login successful!');//
+
+            //passing user and account objects:
+                aptrinsic("identify",
+                {
+                //User Fields
+                "id": username, // Required for logged in app users
+                "email": username,
+               // "firstName": "John",
+               // "lastName": "Smith",
+                "signUpDate": 1522697426479, //unix time in ms
+               // "plan" : "gold", //Custom attributes - please create those custom attributes in Aptrinsic via Account Settings to be tracked.
+               // "price" : 95.5,
+                //"userHash": "" // optional transient for HMAC identification
+                },
+                {
+                //Account Fields
+                "id":"TNSP", //Required
+                "name":"Tinesipo",
+                "Program": "Platinum" // flat custom attributes
+                });
+                
+            window.location.href = 'Home.html';
+            //document.getElementById('TinesipoLoginForm').submit();
+        }
+        }
+    //Gainsight PX Tag
+    (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
+      (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
+        var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
+        var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
+        })
+        (window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-5IWGNRKDCLOF-2");

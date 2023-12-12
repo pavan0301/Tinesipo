63318610a48c3a536e635a0b1aaa9eb5bf19bf6a
pavan0301 <pthallapalli@gainsight.com>
2023-12-12 19:18:46 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,3 +1,13 @@
+    //Gainsight PX Tag
+(function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
+    (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
+      var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
+      var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
+      })
+      (window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-5IWGNRKDCLOF-2");
+
+
+
 function validateForm() {
         var username = document.getElementById('username').value;
         var password = document.getElementById('password').value;
@@ -34,10 +44,5 @@ function validateForm() {
             //document.getElementById('TinesipoLoginForm').submit();
         }
         }
-    //Gainsight PX Tag
-    (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
-      (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
-        var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
-        var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-        })
-        (window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-5IWGNRKDCLOF-2");
+
+    
